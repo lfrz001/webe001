323cebdb4cb7841adb6d0ebcb16c80544955fcfc
Your Name <you@example.com>
2017-08-01 21:30:42 +0800

    2

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-var http = require('http');
+
+var app = express();
 var port = 18080;
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var body = ' \
-        <html> \
-            <head> \
-                <meta charset="utf-8"> \
-                <title>Welcome to BAE</title> \
-            </head> \
-            <body> \
-                <h4>Welcome to Baidu App Engine!</h4> \
-                <h2>欢迎开启BAE之旅！</h2> \
-                <br> \
-                <br> \
-                <ul>以下链接可能对您有用：</ul> \
-                <ul><a href="https://bce.baidu.com/doc/BAE/QuickGuide.html" target="_blank">入门指南</a></ul> \
-                <ul><a href="http://developer.baidu.com/forum/topic/list?boardId=66" target="_blank">论坛</a></ul> \
-                <ul><a href="http://www.baeblog.com/" target="_blank">技术博客</a></ul> \
-                <ul><a href="http://weibo.com/baiduappengine" target="_blank">微博</a></ul> \
-                <ul><a href="https://bce.baidu.com/doc/BAE/FAQ.html" target="_blank">常见问题</a></ul> \
-            </body> \
-        </html> \
-    ';
-    res.write(body);
-    res.end();
-}).listen(port);
+app.use(express.static('public'));
+console.log(__dirname);
+
+
+var server = app.listen(port;, function () {
+
+  //var host = server.address().address
+  //var port = server.address().port
+
+  //console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+});
+app.get('/', function (req, res) {
+   res.send('Hello World');
+});

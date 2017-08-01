9068a115bfaed424ddaae6b9789342079d4ab043
Your Name <you@example.com>
2017-08-01 22:02:04 +0800

    a

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 
+var express = require('express');
 var app = express();
 var port = 18080;
 app.use(express.static('public'));
 console.log(__dirname);
-
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
 var server = app.listen(port, function () {
 
-  //var host = server.address().address
-  //var port = server.address().port
+  var host = server.address().address
+  var port = server.address().port
 
-  //console.log("应用实例，访问地址为 http://%s:%s", host, port)
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
-});
-app.get('/', function (req, res) {
-   res.send('Hello World');
-});
+})
